472163732fbd541864bb2b3faf3258231e3d8036
singam saiprasad <singam.saiprasad@gmail.com>
2026-05-10 17:51:27 +0530

    compete sign api route and add jwt dependency

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from "bcryptjs";
 import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +10,34 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "user created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    console.log(validUser);
+    if (!validUser) return next(errorHandler(404, "user not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword)
+      return next(errorHandler(401, "credentials are not valid"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000);
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expire: expiryDate,
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
