b9f8754108356bd21ab05dc3780bbbe672e43454
singam saiprasad <singam.saiprasad@gmail.com>
2026-05-10 12:50:29 +0530

    create middleware and create a function to handle errors

--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -4,8 +4,5 @@ import { signup } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup);
-router.get("/signup", (req, res) => {
-  res.json({ message: "authserver" });
-});
 
 export default router;
