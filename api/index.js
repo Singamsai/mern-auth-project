e8c341d3612843d59725c9156049ebfe61fc0539
singam saiprasad <singam.saiprasad@gmail.com>
2026-05-13 23:37:50 +0530

    index file bug fix

--- api/index.js
+++ api/index.js
@@ -16,30 +16,36 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-const __dirname = path.resolve();
-const app = express();
-app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+const __dirname = path.resolve();
+
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("server is listening on port 30000");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message;
+
   return res.status(statusCode).json({
     success: false,
     statusCode,
     message,
   });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
