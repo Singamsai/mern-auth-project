90801f1ab74345a64772458997c99c18ae2a0ec7
singam saiprasad <singam.saiprasad@gmail.com>
2026-05-13 09:53:59 +0530

    update the header component and make profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,45 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-    return (
-        <div>
-            <nav className="flex items-center justify-between px-8 py-4 shadow-md">
-                {/* Left Side */}
-                <h1 className="text-2xl font-bold text-blue-600">
-                    Auth App
-                </h1>
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div>
+      <nav className="flex items-center justify-between px-8 py-4 shadow-md">
+        {/* Left Side */}
+        <h1 className="text-2xl font-bold text-blue-600">Auth App</h1>
 
-                {/* Right Side */}
-                <ul className="flex gap-6 text-lg font-medium">
-                    <Link to="/">
-                        <li className="hover:text-blue-600">
-                            Home
-                        </li>
-                    </Link>
-                    <Link to="/about">
-                        <li className="hover:text-blue-600">
-                            About
-                        </li>
-                    </Link>
-                    <Link to="/signup">
-                        <li className="hover:text-blue-600">
-                            SignUp
-                        </li>
-                    </Link>
-                    <Link to="/signin">
-                        <li className="hover:text-blue-600">
-                            SignIn
-                        </li>
-                    </Link>
-                    <Link to="/profile">
-                        <li className="hover:text-blue-600">
-                            Profile
-                        </li>
-                    </Link>
-                </ul>
-            </nav>
-        </div>
-
-    )
+        {/* Right Side */}
+        <ul className="flex gap-6 text-lg font-medium">
+          <Link to="/">
+            <li className="hover:text-blue-600">Home</li>
+          </Link>
+          <Link to="/about">
+            <li className="hover:text-blue-600">About</li>
+          </Link>
+          {currentUser ? null : (
+            <Link to="/signup">
+              <li className="hover:text-blue-600">SignUp</li>
+            </Link>
+          )}
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.profilePic}
+                alt="Profile"
+                className="w-7 h-7 rounded-full object-cover"
+              />
+            ) : (
+              <li className="hover:text-blue-600">SignIn</li>
+            )}
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
 }
